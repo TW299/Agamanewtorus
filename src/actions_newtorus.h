509950258f29b9bc29a2c0c8fec6d42da099d737
TW299 <some5720@ox.ac.uk>
2025-11-14 18:33:00 +0000

    Update actions_newtorus.h

--- src/actions_newtorus.h
+++ src/actions_newtorus.h
@@ -5,10 +5,10 @@
 #include "actions_newgenfnc.h"
 #include "actions_newisochrone.h"
 #include "actions_harmonicoscillator.h"
-#include "actions_focal_distance_finder.h"
 #include "actions_spherical.h"
 #include "potential_base.h"
 #include "potential_utils.h"
+#include "potential_interpolators.h"
 #include "math_core.h"
 #include "math_specfunc.h"
 #include "math_fit.h"
@@ -114,7 +114,6 @@ namespace actions {
 			const PtrToyMap _ptrTM, double _E, bool _negJr, bool _negJz,
 			const PerturbingHamiltonian& _pH) :
 			Torus(_J, _freqs, _GF, _ptrTM, _E, _negJr, _negJz), pH(_pH) {
-			//printf("eTorus created: %d terms in pH\n", pH.numTerms());
 		}
 		eTorus(const Torus& T, const PerturbingHamiltonian& _pH) :
 			Torus(T), pH(_pH) {
@@ -128,11 +127,8 @@ namespace actions {
 		}
 	};
 
-	//interpolate between 2 tori
-	EXP eTorus interpeTorus(const double x, const eTorus& T0, const eTorus& T1);
-
-	//interpolate on an indexed array of tori 
-	EXP eTorus interpeTorus(const double x, std::vector<double>&, std::vector<eTorus>&);
+	/* Interpolate betwee toris with the same ToyMapType */
+	EXP Torus InterpTorus(const double x, const Torus& T0, const Torus& T1);
 
 	/*
 	 * Class for fitting torus to an orbit
@@ -156,12 +152,10 @@ namespace actions {
 	private:
 		const potential::BasePotential& pot;
 		const double defaultTol, invPhi0;
-		math::LinearInterpolator2d interpD; //for Delta values
-		math::LinearInterpolator2d interpR; //for Rshell(L,Xi) values
-		math::LinearInterpolator2d interpV; //for Vshell(L,Xi) values
-		math::LinearInterpolator2d interpRE; //for Rshell(E,Xi) values
-		math::LinearInterpolator interpJz;//Jr(Jr) for box loop orbit transition
+//		math::LinearInterpolator2d interpD; //for Delta values
+//		math::LinearInterpolator2d interpR; //for Rshell(L,Xi) values
 		math::QuinticSpline2d interpJrE;//Esc(Q,Y) for planar orbit, Q=log(Lz+Jr),Y=Lz/(Lz+Jr). Esc=log(1/Phi(0)-1/E)
+//		std::vector<double> JzcritSpl;// Jzcrit(Jfast)
 		std::string logfname;
 #ifdef TEST
 		/* Test_it compares analytic and numerical derivatives */
@@ -174,15 +168,15 @@ namespace actions {
 		 * of the residual H */
 		PerturbingHamiltonian get_pH(const Torus&,
 			int nf, bool ifp, const potential::BasePotential*);
-		//		void setConsts(actions::Actions, double, double&, double&, double&, Isochrone&, coord::UVSph&) const;
-				//ToyMap chooseTM(actions::Actions, double&, double&, double&) const;
+		//void setConsts(actions::Actions, double, double&, double&, double&, Isochrone&, coord::UVSph&) const;
+		//ToyMap chooseTM(actions::Actions, double&, double&, double&) const;
 		int tmax;// Max number of terms retained in residual H
 	public:
 		/* Creator of tori in given potential. GF deemed ok if
 		 * dispersion in H < tol*freqScale*Jtotal */
 		TorusGenerator(const potential::BasePotential& _pot,
 			const double _tol = 1e-9, std::string _logfname = "TG.log");
-		PtrToyMap chooseTM(GenFncFitSeries&, std::vector<double>&, const Actions&,
+		PtrToyMap chooseTM(GenFncFit&, std::vector<double>&, const Actions&,
 			double&, double&, double&, ToyPotType ToyMapType=ToyPotType::None, FILE* logfile = NULL) const;
 		Torus fitTorus(const Actions& J, const double tighten = 1, const ToyPotType ToyMapType=ToyPotType::None) const;
 		/* Fit a torus with all Sn=0 */
@@ -194,30 +188,34 @@ namespace actions {
 		Torus fitFrom(const Actions&, const Torus&, const double tighten = 1) const;
 		eTorus fiteTorus(const Actions&, const potential::BasePotential* _addPhi = NULL);
 		eTorus fiteTorus(const Actions&, const double, const potential::BasePotential* _addPhi = NULL);
-		double getDelta(double&, double&);
-		double getDelta(Actions&);
-		double getRsh(Actions&);
+		double getDelta(double, double) const;
+		double getDelta(const Actions&) const;
+		double getRsh(const Actions&) const;
+		void getRshDelta(const double, const double, double&, double&) const;
 		void test_it(const Actions&, std::vector<double>&);
 		std::vector<Torus> constE(const double Jrmin, const Actions& Jstart, const int Nstep);
 		void old_getHn(const Torus&, int);
+		//interpolate between 2 tori
+		Torus interpTorus(const double x, const Torus& T0, const Torus& T1) const;
+		//interpolae on an indexed array of tori 
+		Torus interpTorus(const double x, const std::vector<double>&, const std::vector<Torus>&) const;
+		//interpolate between 2 tori
+		eTorus interpeTorus(const double x, const eTorus& T0, const eTorus& T1) const;
+		//interpolate on an indexed array of tori 
+		eTorus interpeTorus(const double x, const std::vector<double>&, const std::vector<eTorus>&) const;
 	};
-	//interpolate between 2 tori
-	EXP Torus interpTorus(const double x, const Torus& T0, const Torus& T1, const TorusGenerator *TG=NULL);
-
-	//interpolae on an indexed array of tori 
-	EXP Torus interpTorus(const double x, std::vector<double>&, std::vector<Torus>&, const TorusGenerator *TG);
 
 	class EXP TorusGrid1 {
 	private:
 		const std::vector<double>& xs;
 		const::std::vector<Torus> &Ts;
 		const int nx;
-		const TorusGenerator *TG;
+		const TorusGenerator& TG;
 		int botX(const double) const;
 	public:
 		TorusGrid1(std::vector<double>& _xs,
-			std::vector<actions::Torus> &_Ts, const TorusGenerator *_TG=NULL) :
-			xs(_xs), Ts(_Ts), nx(xs.size()),TG(_TG) {
+			std::vector<actions::Torus> &_Ts, const TorusGenerator& _TG) :
+			xs(_xs), Ts(_Ts), nx(xs.size()), TG(_TG) {
 		}
 		Torus T(const double x) const;
 	};
@@ -226,7 +224,7 @@ namespace actions {
 		const std::vector<double>& xs, ys, zs;
 		const std::vector<Torus>& Ts;
 		const int nx, ny, nz;
-		const TorusGenerator *TG;
+		const TorusGenerator& TG;
 		int botX(const double) const;
 		int botY(const double) const;
 		int botZ(const double) const;
@@ -235,8 +233,8 @@ namespace actions {
 		}
 	public:
 		TorusGrid3(std::vector<double>& _xs, std::vector<double>& _ys,
-			std::vector<double>& _zs, std::vector<actions::Torus>& _Ts, const TorusGenerator *_TG=NULL) :
-			xs(_xs), ys(_ys), zs(_zs), Ts(_Ts), nx(xs.size()), ny(ys.size()), nz(zs.size()),TG(_TG) {
+			std::vector<double>& _zs, std::vector<actions::Torus>& _Ts, const TorusGenerator& _TG) :
+			xs(_xs), ys(_ys), zs(_zs), Ts(_Ts), nx(xs.size()), ny(ys.size()), nz(zs.size()), TG(_TG) {
 		}
 		Torus T(const double x, const double y, const double z) const;
 		Torus T(const Actions J) const {
@@ -278,7 +276,5 @@ namespace actions {
 				std::vector<double>& gridE,
 				std::vector<double>& gridJr,
 				std::vector<double>& gridJz);
-	//EXP void mapJcrit(const potential::BasePotential& pot,
-	//		  math::CubicSpline& Jrcrit, math::CubicSpline& Jzcrit);
 	EXP std::vector<double> mapJcrit(const potential::BasePotential& pot);
 }//namespace actions
